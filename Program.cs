9ee233f2cc942a4d2c1d9d586049cdf9cf673a28
Isaac <isaacgeazy@gmail.com>
2021-03-12 23:37:33 -0300

    Atividade area do triangulo retangulo

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TriangRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ent1, ent2;
+            double h, b, area;
+
+            Console.WriteLine();
+            Console.Write("Digite a Base: ");
+            ent1 = Console.ReadLine();
+            Console.Write("Digite a Altura: ");
+            ent2 = Console.ReadLine();
+
+            h = Convert.ToDouble(ent1);
+            b = Convert.ToDouble(ent2);
+
+            area = (h * b) / 2;
+            
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"A base é {b}");
+            Console.WriteLine($"A altura é {h}");
+            Console.WriteLine();
+            Console.WriteLine($"O valor da área é {area}");
+
+
+        }
+    }
+}
